174e3900c17bc9d2d9d484d8137ff2c69fe66673
AntonShvydkyi <153443213+AntonShvydkyi@users.noreply.github.com>
2024-03-17 22:11:56 +0300

    Employee not enclude in code.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -1,10 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Employee extends Model{
-    protected $table = "employee";
-}
