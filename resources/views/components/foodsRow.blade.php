12999ff82e3ce49ed73835b72683a9e318620d5d
AntonShvydkyi <153443213+AntonShvydkyi@users.noreply.github.com>
2024-03-28 00:24:07 +0300

    Update foodsRow.blade.php

--- resources/views/components/foodsRow.blade.php
+++ resources/views/components/foodsRow.blade.php
@@ -7,14 +7,3 @@
 
 
 
-{{--
-<div class="row">
-    <div class="col">
-        {{$animalName}}
-    </div>
-    <div class="col">
-        @foreach($employees as $employee)
-            <a href="{{ route('employees_data', ['employees_id' => $employee->animal_id]) }}">Посилання</a>
-        @endforeach
-    </div>
-</div>--}}
